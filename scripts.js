44db6cb38bdf2821d4f9c22a7e45f55f9b780559
eamend28 <117699959+eamend28@users.noreply.github.com>
2022-11-17 20:03:52 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,2 +1,17 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+// Write your JavaScript code here.
+// Remember to pay attention to page loading!
+window.addEventListener("load", function (){
+const takeOffButton = document.getElementById("takeoff")
+const flightStatusTag = document.getElementById("flightStatus")
+takeOffButton.addEventListener("click" , function() {
+    alert('Confirm that the shuttle is ready for takeoff.');
+//flightStatusTag.innerHTML += 'Shuttle in flight.'
+
+});
+
+
+
+
+})
